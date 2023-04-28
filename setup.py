608a3e805c2cb6b7cddf68f9a9fcfaf5c2a3449a
diogo <diogo.correia01@outlook.com>
2023-04-28 18:18:41 +0100

    First Commit

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
-from codecs import open
-from os.path import join, abspath, dirname
-from setuptools import setup
-
-here = abspath(dirname(__file__))
-
-with open(join(here, 'README.md'), encoding='utf-8') as buff:
-    long_description = buff.read()
+from setuptools import setup, find_packages
 
 setup(
     name="TextProcessing",
     version="1.0",
     description="Projeto 2 - TAP",
-    long_description_content_type="text/markdown",
-    author="Diogo Correia"
-           "João Alexandre",
     install_requires=[
         'spacy',
         'cleantext',
         'nltk',
         'bs4'
-    ]
+    ],
+    url='https://github.com/diogocorreia01/TextProcessing.git',
 )
